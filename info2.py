6adee13a67150f6f696d6eaf5b6c7a76d460d825
MichaelKolby1 <michaelkolby623@gmail.com>
2024-11-22 20:02:27 -0500

    info2.py

--- info2.py
+++ info2.py
@@ -17,11 +17,11 @@ def fortniteSkins():
         itemType = skins["type"]
         values = itemType["value"]
         if values.lower() == "outfit":
-        	if name != "TBD":
+        	if name != "TBD" or name != "null":
                     skinNameList.append(name)
 
-    randomNumber = random.randrange(1,11)
-    for i in range(randomNumber,2200,200):
+    randomNumber = random.randrange(0,200)
+    for i in range(randomNumber,2000,200):
         chosenSkin = skinNameList[i]
         finalList.append(chosenSkin)
     for skins in skinData:
@@ -38,4 +38,53 @@ def fortniteSkins():
             if chosenSkins == name:
                 skinsDict[name] = [rarityValue, chapter, skinImage]
 
+
 print(fortniteSkins())
+
+bannerList = []
+def fortniteBanners():
+    global bannerList
+    websiteUrl = "https://fortnite-api.com/v1/banners"
+    fullAPI = requests.get(websiteUrl)
+    cosmetics = fullAPI.json()
+
+    bannerData = cosmetics["data"]
+    for banners in bannerData:
+        name = banners["name"]
+        if "images" in banners:
+            imagesKey = banners["images"]
+            if "icon" in imagesKey:
+                image = imagesKey["icon"]
+        bannerList.append(image)
+    
+    finalBannerList = []
+    ranNum = random.randrange(0,251)
+    for i in range(ranNum,753,250):
+        chosenBanner = bannerList[i]
+        finalBannerList.append(chosenBanner)
+    
+    
+print(fortniteBanners())
+
+
+finalLocationList = []
+def fortnitePOIS():
+    global finalLocationList
+    websiteUrl = "https://fortnite-api.com/v1/map"
+    fullAPI = requests.get(websiteUrl)
+    locations = fullAPI.json()
+
+    mapData = locations["data"]
+    pois = mapData["pois"]
+    poiList = []
+    for locations in pois:
+        name = locations["name"]
+        poiList.append(name)
+
+    ranNum3 = random.randrange(0,27)
+    for i in range(ranNum3,83,27):
+        chosenLocation = poiList[i]
+        if chosenLocation[0].isalpha():
+            finalLocationList.append(chosenLocation)
+
+print(fortnitePOIS())
